e1e5031782271ca7d3ddee844af6b6523d004db9
eddiwar <edwar.zhang1@gmail.com>
2017-09-25 17:08:35 +1300

    fixed htk code logic and uncommented hard code

--- src/a03/Number.java
+++ src/a03/Number.java
@@ -1,7 +1,7 @@
 package a03;
 
 public enum Number {
-	ONE("tahi", 1), TWO("rua", 2), THREE("toru", 3), FOUR("whaa", 4), FIVE("Rima", 5), SIX("ono", 6), SEVEN("whitu", 7), EIGHT("waru", 8), NINE("iwa", 9), TEN("tekau", 10);
+	ONE("tahi", 1), TWO("rua", 2), THREE("toru", 3), FOUR("whaa", 4), FIVE("rima", 5), SIX("ono", 6), SEVEN("whitu", 7), EIGHT("waru", 8), NINE("iwa", 9), TEN("tekau", 10);
 	
 	private final String maoriName;
 	private final int number;
