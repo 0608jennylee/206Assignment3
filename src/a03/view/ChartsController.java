49584f353d649d40c75434f639eae410168df6e0
Jenny Lee <jlee923@aucklanduni.ac.nz>
2017-10-11 22:49:48 +1300

    completed scoreboard and chart buttons and scene layout

--- src/a03/view/ChartsController.java
+++ src/a03/view/ChartsController.java
@@ -1,8 +1,12 @@
 package a03.view;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 
+import java.io.File;
+import java.net.URL;
 import java.util.Arrays;
+import java.util.ResourceBundle;
 
 import com.jfoenix.controls.JFXButton;
 
@@ -14,19 +18,22 @@ import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
-public class ChartsController {
+public class ChartsController extends Controller implements Initializable{
 	@FXML
 	private JFXButton _back;
 	@FXML
 	private BarChart<String, Number> _barChart;
 	@FXML
-	private JFXButton _left;
+	private JFXButton _previous;
 	@FXML
-	private JFXButton _right;
+	private JFXButton _next;
 	private MainApp _mainApp;
 	private int _currentChart;
-
+	@FXML private ImageView _imageView;
+	@FXML private NumberAxis _yAxis;
 	// Event Listener on JFXButton[#_back].onAction
 	@FXML
 	public void handleBack(ActionEvent event) {
@@ -63,14 +70,36 @@ public class ChartsController {
 
 		XYChart.Series<String, Number> series2 = new XYChart.Series<>();
 		series2.setName("scores");
-		series2.getData().add(new XYChart.Data<>("Speed", 5.0));
-		series2.getData().add(new XYChart.Data<>("User rating", 6.0));
-		series2.getData().add(new XYChart.Data<>("Milage", 10.0));
-		series2.getData().add(new XYChart.Data<>("Safety", 4.0));
+		series2.getData().add(new XYChart.Data<>("a", 50.0));
+		series2.getData().add(new XYChart.Data<>("b", 80.0));
+		series2.getData().add(new XYChart.Data<>("c", 100.0));
+		series2.getData().add(new XYChart.Data<>("d", 20.0));
 
 		//Setting the data to bar chart       
 		_barChart.getData().addAll(series2);
 		_barChart.lookupAll(".default-color0.chart-bar")
 		.forEach(n -> n.setStyle("-fx-bar-fill: orange;"));
+	}
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		//		_anchor.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+		_yAxis.setAutoRanging(false);
+		_yAxis.setLowerBound(0);
+		_yAxis.setUpperBound(100);
+		_yAxis.setTickUnit(10);
+		File file4 = new File(System.getProperty("user.dir")+"/Icons/png/quit.png");
+		Image image4 = new Image(file4.toURI().toString());
+		_back.setGraphic(new ImageView(image4));
+		File file1 = new File(System.getProperty("user.dir")+"/Icons/png/arrow-circle-left-8x.png");
+		Image image1 = new Image(file1.toURI().toString());
+		_previous.setGraphic(new ImageView(image1));
+		File file3 = new File(System.getProperty("user.dir")+"/Icons/png/arrow-circle-right-8x.png");
+		Image image3 = new Image(file3.toURI().toString());
+		_next.setGraphic(new ImageView(image3));
+		File file5 = new File(System.getProperty("user.dir")+"/fern.jpg");
+		Image image5 = new Image(file5.toURI().toString());
+		_imageView.setImage(image5);
+		_imageView.setOpacity(0.1);
+
 	}
 }
--- src/a03/view/ScoreBoardController.java
+++ src/a03/view/ScoreBoardController.java
@@ -3,16 +3,20 @@ package a03.view;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import a03.GameStats;
 import a03.MainApp;
 import javafx.event.ActionEvent;
-
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Labeled;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
-public class ScoreBoardController{
+public class ScoreBoardController extends Controller implements Initializable{
 	@FXML private Label _easyHoursPlayed;
 	@FXML private Label _easyTopScore;
 	@FXML private Label _EasyLowestScore;
@@ -21,22 +25,28 @@ public class ScoreBoardController{
 	@FXML private Label _hardTopScore;
 	@FXML private Label _hardLowestScore;
 	@FXML private Label _hardAverageScore;
-	private MainApp _mainApp;
+	@FXML private Button _back;
+	@FXML private ImageView _imageView;
 
 	// Event Listener on Button.onAction
 	@FXML
 	public void handleBack(ActionEvent event) {
 		_mainApp.mainMenuContents();
 	}
-	
-	public void setMainApp(MainApp mainApp){
-		_mainApp = mainApp;
-	}
 
-//	@Override
-//	public void initialize(URL location, ResourceBundle resources) {
-//		setScores();
-//	}
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		//		_anchor.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+
+		File file4 = new File(System.getProperty("user.dir")+"/Icons/png/quit.png");
+		Image image4 = new Image(file4.toURI().toString());
+		_back.setGraphic(new ImageView(image4));
+		File file5 = new File(System.getProperty("user.dir")+"/fern.jpg");
+		Image image5 = new Image(file5.toURI().toString());
+		_imageView.setImage(image5);
+		_imageView.setOpacity(0.05);
+
+	}
 	
 	public void setScores(){
 		_easyTopScore.setText(GameStats.getGameStats().getEasyHighestScore().toString());
