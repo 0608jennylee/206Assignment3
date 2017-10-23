9b44733fc58c9f3afb0751b8ad6bd24f5cfaab9d
eddiwar <edwar.zhang1@gmail.com>
2017-10-23 16:57:39 +1300

    fixed starting new game when there is a save file not deleting the save

--- src/a03/view/LoadLevelController.java
+++ src/a03/view/LoadLevelController.java
@@ -43,6 +43,7 @@ public class LoadLevelController implements Initializable{
 	// Event Listener on JFXButton[#_hard].onAction
 	@FXML
 	public void handleNew(ActionEvent event) {
+		new File("Saves/" + _level.toString() + _difficulty.toString() + ".dat").delete();
 		_mainApp.Start(_level, _difficulty, 10);
 	}
 	public void setMainApp(MainApp mainApp) {
