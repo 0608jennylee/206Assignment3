22874dee0c89be72c979bb29664e84b9e4e77380
eddiwar <edwar.zhang1@gmail.com>
2017-10-26 21:39:44 +1300

    fixed error box image resizing and made button style consistent

--- src/a03/view/ErrorBoxController.java
+++ src/a03/view/ErrorBoxController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class ErrorBoxController extends Controller{
@@ -11,6 +12,7 @@ public class ErrorBoxController extends Controller{
 	@FXML Label _text;
 	@FXML ImageView _logo;
 	@FXML ImageView _imageView;
+	@FXML AnchorPane _root;
 	
 	
 	private Stage _errorStage;
@@ -32,7 +34,7 @@ public class ErrorBoxController extends Controller{
 	private void init() {
 		Image quit = new Image(getClass().getClassLoader().getResource("Icons/error.png").toString());//
 		Image background = new Image(getClass().getClassLoader().getResource("fern.jpg").toString());//
-		_imageView.setFitWidth(621);
+		_imageView.setFitWidth(_root.getWidth());
 		_imageView.setImage(background);
 		_logo.setImage(quit);
 		_imageView.setOpacity(0.1);
